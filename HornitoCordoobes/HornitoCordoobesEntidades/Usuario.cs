393932aedadb3667dfbc89b9082545211e77f5b2
mcapodacqua <mcapodacqua@outlook.com>
2014-09-27 18:45:55 -0300

    Cambiando nombre de propiedad de usuario

--- HornitoCordoobes/HornitoCordoobesEntidades/Usuario.cs
+++ HornitoCordoobes/HornitoCordoobesEntidades/Usuario.cs
@@ -9,7 +9,7 @@ namespace HornitoCordoobesEntidades
     public class Usuario
     {
         public int Id { get; set; }
-        public string Username { get; set; }
+        public string NombreUsuario { get; set; }
         public string Password { get; set; }
         public string Rol { get; set; }
     }
