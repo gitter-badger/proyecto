8362c919061f1603ce78c5cb51d72648eebf3bd3
mcapodacqua <mcapodacqua@outlook.com>
2014-09-27 17:35:11 -0300

    agregando entidades

---
+++ HornitoCordoobes/HornitoCordoobesEntidades/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HornitoCordoobesEntidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Rol { get; set; }
+    }
+}
