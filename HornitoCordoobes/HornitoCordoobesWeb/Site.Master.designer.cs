89abdb950bdcc2012595e9e137c5a1e03ffc23de
mcapodacqua <mcapodacqua@outlook.com>
2014-09-28 18:22:35 -0300

    Agregando menu

--- HornitoCordoobes/HornitoCordoobesWeb/Site.Master.designer.cs
+++ HornitoCordoobes/HornitoCordoobesWeb/Site.Master.designer.cs
@@ -31,6 +31,24 @@ namespace HornitoCordoobesWeb {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// Menu1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu Menu1;
+        
+        /// <summary>
+        /// SiteMapDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapDataSource SiteMapDataSource1;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
