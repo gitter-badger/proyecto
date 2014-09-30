1cb00c82b5988a2730bfc22841458cfc396823e0
Mauricio Domenech <mauriciodomenech@icloud.com>
2014-09-30 15:43:59 -0300

    Agrego clase con el String de conexión para que la tengas y le pongas tu String de conexión, despues hay que hacer el ignore para que no se vuelva a subir

---
+++ HornitoCordoobes/HornitoCordoobesNegocio/Utils/ConnetionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HornitoCordoobesNegocio.Utils
+{
+    class ConnetionString
+    {
+        public static string connetionString = "Data Source=MAURI-PC;Initial Catalog=HornitoCoordobes;Integrated Security=True";
+    }
+}
